a0b8ce84c5239f7217bf01010812560b86ec7166
John Bain <johnknoxbain@gmail.com>
2020-08-24 09:47:20 -0500

    added user and customer api functionality

--- bangazonllc/urls.py
+++ bangazonllc/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
+from bangazonapp.views import Customers, UserViewSet
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'customer', Customers, 'customer')
+router.register(r'users', UserViewSet, 'user')
+
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
+
