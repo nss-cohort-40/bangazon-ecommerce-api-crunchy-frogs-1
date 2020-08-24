1e2c24cd98d2f8175b2c8357bc763c925631345b
felipetempus <felipemouradev@gmail.com>
2020-08-24 14:40:41 -0400

    working ModelSerializer

--- bangazonapp/views/product.py
+++ bangazonapp/views/product.py
@@ -4,9 +4,7 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
-from django.contrib.auth.hashers import make_password
-from django.contrib.auth.models import User
-from bangazonapp.models import Product
+from bangazonapp.models import Product, Customer, ProductType
 
 
 # Table Product {
@@ -22,14 +20,14 @@ from bangazonapp.models import Product
 #   ProductTypeId int
 # }
 
-class ProductSerializer(serializers.HyperlinkedModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        url = serializers.HyperlinkedIdentityField(
-            view_name='customer',
-            lookup_field='id'
-        )
-        fields = ('id', 'title', 'price', 'description', "quantity", "location", "image_path", "product_type")
+        # url = serializers.HyperlinkedIdentityField(
+        #     view_name='products',
+        #     lookup_field='id'
+        # )
+        fields = ("id", "title", "price", "description", "quantity", "location", "image_path", "product_type", "customer")
         depth = 2
 
 class Products(ViewSet):
@@ -39,82 +37,83 @@ class Products(ViewSet):
         """Handle POST operations
 
         Returns:
-            Response -- JSON serialized User instance
+            Response -- JSON serialized Product instance
         """
+        customer = Customer.objects.get(pk=request.data["customer"])
+        product_type  = ProductType.objects.get(pk=request.data["product_type"])
 
         product = Product.objects.create(
             title = request.data["title"],
-            last_name = request.data["price"],
+            price = request.data["price"],
             description = request.data["description"],
             quantity = request.data["quantity"],
             location = request.data["location"],
             image_path = request.data["image_path"],
-            product_type_id = request.data["product_type_id"],
+            product_type = product_type,
+            customer = customer
         )
+        serializer = ProductSerializer(product, context={'request': request})
 
-        return HttpResponse(data, content_type='application/json')
+        return Response(serializer.data, content_type='application/json')
 
     def retrieve(self, request, pk=None):
-        """Handle GET requests for single park area
+        """Handle GET operation
 
         Returns:
-            Response -- JSON serialized park area instance
+            Response -- JSON serialized product instance
         """
         try:
             # user = User.objects.get(pk=pk)
-            customer = Customer.objects.get(pk=pk)
-            serializer = CustomerSerializer(customer, context={'request': request})
+            product = Product.objects.get(pk=pk)
+            serializer = ProductSerializer(product, many=False, context={'request': request})
             return Response(serializer.data)
         except Exception as ex:
             return HttpResponseServerError(ex)
 
     def update(self, request, pk=None):
-        """Handle PUT requests for a park area
+        """Handle PUT requests for products
 
         Returns:
             Response -- Empty body with 204 status code
         """
 
-        customer = Customer.objects.get(pk=pk)
-        customer.address = request.data["address"]
-        customer.phone_number = request.data["phone_number"]
-        customer.save()
+        product = Product.objects.get(pk=pk)
+        product.title = request.data["title"],
+        product.price = request.data["price"],
+        product.description = request.data["description"],
+        product.quantity = request.data["quantity"],
+        product.location = request.data["location"],
+        product.image_path = request.data["image_path"],
+        product.product_type = request.data["product_type"]
+        product.save()
 
-        user = User.objects.get(pk=customer.user.id)
-        user.first_name = request.data["first_name"]
-        user.last_name = request.data["last_name"]
-        user.username = request.data["username"]
-        user.password = make_password(request.data["password"])
-        user.email = request.data["email"]
-        user.save()
 
         return Response({}, status=status.HTTP_204_NO_CONTENT)
 
     def destroy(self, request, pk=None):
-        """Handle DELETE requests for a single park are
+        """Handle DELETE requests for a product
 
         Returns:
             Response -- 200, 404, or 500 status code
         """
         try:
-            user = User.objects.get(pk=pk)
-            user.delete()
+            product = Product.objects.get(pk=pk)
+            product.delete()
 
             return Response({}, status=status.HTTP_204_NO_CONTENT)
 
-        except Customer.DoesNotExist as ex:
+        except Product.DoesNotExist as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
 
-        except Exception as ex:
-            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        # except Exception as ex:
+        #     return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def list(self, request):
-        """Handle GET requests to park areas resource
+        """Handle GET requests to product resource
 
         Returns:
-            Response -- JSON serialized list of park areas
+            Response -- JSON serialized list of products
         """
-        customers = Customer.objects.all()  # This is my query to the database
-        serializer = CustomerSerializer(
-            customers, many=True, context={'request': request})
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True, context={'request': request})
         return Response(serializer.data)
